9fd9c3567203d81ecaf6bb6d159554df4e11d147
wuing2 <835757447@qq.com>
2016-06-30 16:27:28 +0800

    the practice class3 by one to many relationship

--- manage.py
+++ manage.py
@@ -1,8 +1,9 @@
 #   -*- encoding=UTF-8 -*-
 import  random
-from  nowstagram import  app,db
-from nowstagram.models import  User,Images,Comment
+from  nowstagram import  app, db
+from nowstagram.models import  User, Images, Comment
 from  flask_script import  Manager
+# import  operator
 
 manager = Manager(app)
 
@@ -14,20 +15,70 @@ def init_database():
     db.drop_all()
     db.create_all()
     for i in range(1, 100):
-        db.session.add(User("practice"+str(i), 'pass'+str(i)))
+        db.session.add(User('practice'+str(i), 'pass'+str(i)))
     #   密码是需要加密的
         for j in range(0, 3):
             db.session.add(Images(get_url(), i+1))
             for k in range(0, 3):
-                # db.session.add(Comment('这是一条评论', random.randint(0,100),random.randint(0,100)))
+                # db.session.add(Comment('这是一条评论'+str(k), random.randint(0,100),random.randint(0,100)))
                 db.session.add(Comment('this is a comment', 1 + 3 * i + j, i + 1))
+#                 需要将中文字符串转换成 buffer() 类型才不会有问题
+#                 sqlite3针对BLOB（二进制大对象）的存储时默认为buffer类型的，所以需要进行转换
+#                 可是这样数据库里面显示的就不是中文了啊
 #             出现中文的字符会出错，错误提示如下：
 # You must not use 8-bit bytestrings unless you use a text_factory that can interpret 8-bit bytestrings (like text_factory = str). It is highly recomme
 # nded that you instead just switch your application to Unicode strings. [SQL: u'INSERT INTO comment (user_id, content, image_id, status) VALUES (?, ?, ?, ?)'] [parameters: ('\xe8\xbf\x99this is a comment', 4, 2
 # , 1)]
 #             即这个数据库的字符串不支持中文字符
-
     db.session.commit()
+
+    for i in range(0, 100, 2):
+        User.query.filter_by(id=i+1).update({'username':'prac'+str(i+1)})
+    # 这个确实更新了数据库，通过query查询到的数据时更新了的，但是用数据库打开却看不到更新，不知道为什么
+    # 因为没有提交commit，哈哈哈哈哈
+    db.session.commit()
+
+    # for i in range(0, 100, 3):
+    #     p = User.query.get(i+1)
+    #     p.username = 'u'+str(i+1)
+    #     # 通过属性设置会出错？错误描述： 'NoneType' object has no attribute 'username'
+    #     #为什么识别为 NONETYPE ？
+    # db.session.commit()
+
+    for i in range(0,50,3):
+        User.query.filter_by(id=i+2).delete()
+    #     直接对查询的到结果进行删除
+    for i in range(50,100,4):
+        comment =Comment.query.filter_by(id=i+1).first()
+        # comment = Comment.query.get(i)
+        # 对查询到的结果进行会话删除，删除对象每次只能提交一个，不能提交多个
+        # filter_by()得到的结果是一个列表，虽然这个列表可能只有一个元素，但仍然通不过 .first()则提交的不是列表了
+        db.session.delete(comment)
+    db.session.commit()
+
+
+    print 1, User.query.all( )
+    print 2, User.query.get(10)
+    print 3, User.query.filter_by(id=5).first()
+    print 4, User.query.order_by(User.id.desc()).offset(1).limit(4).all()
+    print 5, User.query.paginate(page=2, per_page=10).items
+    # 分页显示
+    # print 5, User.query.filter(or_(User.id== 3,User.id == 88)).all()
+    #or_ and_ 这些组合操作符否不可用，不知从哪里导入
+    u = User.query.get(7)
+    print 6,u.username
+    #为何这边却可以这样用呢？还是因为名字冲突了，不是名称的问题啊
+    #一对多查询
+    # print  7, u.comment
+    print  72,u.images.all()
+    # 在没有设置backref的情况下，可以正确的得到关联的images结果，但backref后就没有了
+    # 有结果，但是直接打印出出来格式不对，用.all()就可以全部打印出来
+    print 8,Images.query.get(5).user
+#     为什么结果显示仍然为空？因为查询的哪一项不存在，被前面删除了
+    c = Comment.query.get(7)
+    print 9, c
+    print 10, c.user
+    print  11,c.image_id
 
 if __name__ == '__main__':
     manager.run()
--- nowstagram/models.py
+++ nowstagram/models.py
@@ -3,14 +3,25 @@ import random
 from datetime import  datetime
 from nowstagram import db
 
+# 关于一对多关系的解释举例：在User类中存在images属性与Images类关联起来，但是在User类创建时Images类
+# 并不存在，也就是创建之间存在先后关系，在先创建的类User中使用images占位符来代替暂未创建的Images中的属性
+# 在创建Images类时使用外键将user_id与User类关联起来，因为在User类中使用了db.relationship关联了Images，
+# 故而可以用images属性查询Images表中的相关内容，而Images类并没有与User类用relationship关联，故而不能方向查询
+# 但是在使用了backref后则可以反向查询User类中相关信息，查询关键字时backref=的那个
 
+# 会自动将类名转换成小写然后作为表名，，，所以再外键时使用的是表名，而不是类名
+#但可以通过  __tablename__ = 'myuser' 来指定表名字
 class User(db.Model):
+    __table_args__ = {'mysql_collate': 'utf8_general_ci'}
     # id = db.column(db.Integer, primary_key=True, autoincremet=True)
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    # 可以由多个主键
     username = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(32))
     head_url = db.Column(db.String(256))
-    image =  db.relationship('Images')
+    comment = db.relationship('Comment',backref='user',lazy='dynamic')
+    # images = db.relationship('Images')
+    images =  db.relationship('Images', backref='user', lazy='dynamic')
 
     def __init__(self, username, password):
         self.username = username
@@ -22,12 +33,14 @@ class User(db.Model):
 
 
 class Images(db.Model):
+    __table_args__ = {'mysql_collate': 'utf8_general_ci'}
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    # db.ForeignKey('user.id') 而不能是 'User.id' 否则会找不到，因为默认表名是类名的小写
     url = db.Column(db.String(512))
     # comment = db.Column(db.String(1024))
     created_date = db.Column(db.DateTime)
-    # comments = db.relationship('Comment')
+    comment = db.relationship('Comment')
 
     def __init__(self, url, user_id):
         self.url = url
@@ -39,16 +52,18 @@ class Images(db.Model):
 
 
 class Comment(db.Model):
+    __table_args__ = {'mysql_collate': 'utf8_general_ci'}
+    # 增加对中文的支持,可是我增加了这条之后仍然无法支持中文啊
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    user_id = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     content = db.Column(db.String(1024))
-    image_id = db.Column(db.Integer)
+    image_id = db.Column(db.Integer,db.ForeignKey('images.id'))
     status = db.Column(db.Integer, default=0)
     # user =  db.relationship('User')
 
-    def __init__(self, user_id, content, image_id):
-        self.user_id = user_id
+    def __init__(self, content, user_id,  image_id):
         self.content = content
+        self.user_id = user_id
         self.image_id = image_id
         # self.status = status
 
